95d554ed93caf720208454cdb226c8e515f3d39e
Adura-Hephzibah <adurahephzibah@gmail.com>
2023-01-22 20:52:11 +0200

    test files

---
+++ tests/test_models/test_base_model.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Test module
+"""
+
+import unittest
+from models.base_model import BaseModel
+from datetime import datetime
+from io import StringIO
+from contextlib import redirect_stdout
+
+
+class TestBaseModel(unittest.TestCase):
+    """Test"""
+
+    def test_to_dict(self):
+        """test to_dict method"""
+
+        copy = BaseModel()
+        copy_dict = copy.to_dict()
+        self.assertIsInstance(copy_dict["created_at"], str)
+
+    def test_unique_id(self):
+        """test id uniqueness"""
+        copy = BaseModel()
+        copy_2 = BaseModel()
+        self.assertNotEqual(copy.id, copy_2.id)
+
+    def test_created_at(self):
+        """test created_at attribute"""
+        copy = BaseModel()
+        self.assertIsInstance(copy.created_at, datetime)
+
+    def test_str(self):
+        """test str method"""
+        with StringIO() as bufr, redirect_stdout(bufr):
+            my_cop = BaseModel()
+            print(my_cop.__str__())
+            a = bufr.getvalue()
+        self.assertIn(
+            f'[{my_cop.__class__.__name__}] ({my_cop.id}) {my_cop.__dict__}',
+            a)
+
+    def test_save(self):
+        """test save()"""
+        copy_1 = BaseModel()
+        copy_1.name = "James"
+        copy_1.save()
+        self.assertNotEqual(copy_1.created_at, copy_1.updated_at)
+
+
+if __name__ == "__main__":
+    unittest.main()
---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,66 @@
+#!/usr/bin/python
+"""
+Test for file storage
+"""
+
+import unittest
+from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
+import os
+
+
+class TestFileStorage(unittest.TestCase):
+    """test"""
+    def setUp(self):
+        """setup"""
+        self.base = BaseModel()
+        self.store = FileStorage()
+    
+    def teardown(self):
+        """teardown"""
+        try:
+            os.remove("file.json")
+        except:
+            pass
+        del self.base
+        del self.store
+    
+    def test_all(self):
+        """test"""
+        store_dict = self.store.all()
+        self.assertIsInstance(store_dict, dict)
+    
+    def test_news(self):
+        """test"""
+        for item in self.store.all().values():
+            same = item
+        self.assertTrue(same is item)
+    
+    def test_saver(self):
+        """saver"""
+        self.base.save()
+        self.assertTrue(self.store._FileStorage__objects.get(f"BaseModel.{self.base.id}"))
+
+    # def test_reload(self):
+    #     """reload"""
+    #     self.store.save()
+    #     self.store.reload()
+    #     for obj in self.store.all().values():
+    #         load = obj
+    #     self.assertEqual(self.base.to_dict()['id'], load.to_dict()['id'])
+
+    def test_model(self):
+        """model"""
+        # with open('file.json', 'w') as f:
+        #     pass
+        # with self.assertRaises(ValueError):
+        #     self.store.reload()
+        # with open("file.json", "w") as f:
+        #     f.write("{}")
+        # with open("file.json", "r") as r:
+        #     for line in r:
+        #         self.assertEqual(line, "{}")
+        # self.assertIs(self.store.reload(), None)
+        self.store.save()
+        self.store.reload()
+        self.assertTrue(len(self.store.all()) > 0)
