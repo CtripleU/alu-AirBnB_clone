84a8efc98533e0ecb6bd32fa4bf4b4cb2d98ecd5
CtripleU <cuoyemike@gmail.com>
2023-01-24 15:25:30 +0200

    create a user class with its attributes

---
+++ models/user.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# from uuid import uuid4
+# from datetime import datetime
+# import models
+
+from models.base_model import BaseModel
+
+"""
+Module User
+defines a user class with attributes including email, password, first_name, and last_name
+"""
+
+
+class User(BaseModel):
+    """
+    User class
+    """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
