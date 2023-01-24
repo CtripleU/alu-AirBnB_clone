aff7a523ac92b006a5293d05c93ce64cb1324a6f
CtripleU <cuoyemike@gmail.com>
2023-01-24 20:19:12 +0200

    created amenity class

---
+++ tests/test_models/test_amenity.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+"""
+Module Amenity
+defines the State class with a public attribute 'name'
+"""
+
+
+class Amenity(BaseModel):
+    """
+    Amenity class
+    """
+    name = ""
